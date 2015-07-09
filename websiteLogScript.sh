b5519620cd97db899411e4a1e1fc8f7ce5ac4d00
jawilson0502 <jwils117@emich.edu>
2015-07-09 13:56:29 -0400

    Updated Web Log Script to better filter through views

--- websiteLogScript.sh
+++ websiteLogScript.sh
@@ -2,31 +2,56 @@
 
 #Created by Jessica Wilson July 5th 2015
 
+#pull out any ip's that only see a single page w/o loading css (which is a 2nd page load- assuming it is a bad bot
+cat /var/log/apache2/access.log | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq -c | sort -n > IPCounts.txt;
+
+#make sure there is no residual file, there may not be a file, so this creates one incase there isn't to remove
+touch badBots.txt;
+rm badBots.txt;
+
+while read a b;
+do 
+	if [ $a -eq 1 ]
+		then echo $b >> badBots.txt;
+	fi
+done < IPCounts.txt;
+
 #pull all requests for pages not related to js/css
 cat /var/log/apache2/access.log | grep -v -E 'js|css|favicon|img|fonts'  > accessedPages.txt;
 
 #find bots out of those pages assuming that only robots visit robots.txt
-cat accessedPages.txt | grep -E 'robots.txt' |grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq >> knownBots.txt ;
-cat knownBots.txt |sort -n |uniq > knownBots.txt
+cat accessedPages.txt | grep -E 'robots.txt' |grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq > knownBots.txt ;
 
 #filter out all known robots
-cat accessedPages.txt | grep -v -f "knownbots.txt" > reducedHits.txt;
+cat accessedPages.txt | grep -v -f "knownBots.txt" | grep -v -f "badBots.txt" > reducedHits.txt;
 
 #Assume that if someone gets a 404 , they maliciously went there
 cat reducedHits.txt | grep 404 > badVisits.txt;
-cat badVisits.txt | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq > badBots.txt;
+cat badVisits.txt | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq >> badBots.txt;
 
 #filter out all malicious visits
 cat reducedHits.txt |grep -v -f "badBots.txt" | grep -o '.*HTTP' | grep -v 'OPTIONS' | grep -v -E "127.0.0.1|10.0.0" > actualHits.txt;
 
 #pull out IP's for good visitors
-cat actualHits.txt | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq > goodVisitors.txt;
+cat actualHits.txt | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq -c | sort -n> goodVisitors.txt;
 
 #find out where the IP's are located from good visitors
-while read a;
+
+#rm an old file if it exists, creates it if it doesn't and removes it to ensure we start fresh
+touch visitorsLoc.txt;
+rm visitorsLoc.txt;
+
+while read a b;
 do 
-	b=$(echo `curl -s ipinfo.io/$a/city`);
-	c=$(echo `curl -s ipinfo.io/$a/region`);
-	d=$(echo `curl -s ipinfo.io/$a/country`);
-	echo $a $b $c $d >> visitorsLoc.txt;
+	c=$(echo `curl -s ipinfo.io/$b/city`);
+	d=$(echo `curl -s ipinfo.io/$b/region`);
+	e=$(echo `curl -s ipinfo.io/$b/country`);
+	echo $a $b $c $d $e >> visitorsLoc.txt;
 done < goodVisitors.txt
+
+#remove files I don't want to review afterwards
+rm goodVisitors.txt;
+rm accessedPages.txt;
+rm actualHits.txt;
+rm badVisits.txt;
+rm reducedHits.txt;

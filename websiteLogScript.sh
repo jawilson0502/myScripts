c9498c2dae4f27d0d8b2308e631e86d96c60fd0c
thearchdukeofdoge <jeffkaminski98@yahoo.com>
2015-09-21 21:12:20 -0400

    Update
    Added a section for ua string decoding and cleaned up a bit

--- websiteLogScript.sh
+++ websiteLogScript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #Created by Jessica Wilson July 5th 2015
-#Updated by Jessica Wilson July 13th 2015
-#look an edited comment
+#Updated by Jessica Wilson July 15th 2015
+#Updated by Jeff Kaminski September 21st 2015
+
+#Declare the api key for useragentapi.com, get your from http://useragentapi.com/account/signup
+apikey="<getyour own api>"
 
 #pull out any ip's that only see a single page w/o loading css (which is a 2nd page load- assuming it is a bad bot
 cat /var/log/apache2/access.log | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq -c | sort -n > IPCounts.txt;
@@ -36,7 +39,7 @@ cat reducedHits.txt | grep -E '404|proxy|seo|Synapse' > badVisits.txt;
 cat badVisits.txt | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq >> badBots.txt;
 
 #filter out all malicious visits
-cat reducedHits.txt |grep -v -f "badBots.txt" | grep -o '.*HTTP' | grep -v 'OPTIONS' | grep -v -E "127.0.0.1|10.0.0" > actualHits.txt;
+cat reducedHits.txt |grep -v -f "badBots.txt" | grep -v 'OPTIONS' | grep -v -E "127.0.0.1|10.0.0" > actualHits.txt;
 
 #pull out IP's for good visitors
 cat actualHits.txt | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq -c | sort -n> goodVisitors.txt;
@@ -48,12 +51,28 @@ rm visitorsLoc.txt;
 
 while read a b;
 do 
-	curl -s ipinfo.io/$b/geo > temp.txt;
+	#The original which stored to txt, remove this
+	#curl -s ipinfo.io/$b/geo > temp.txt;
+	geo=$(curl -s ipinfo.io/$b/geo)
 	
-	c=$(echo `cat temp.txt | jq '.city'`);
-	d=$(echo `cat temp.txt | jq '.region'`);
-	e=$(echo `cat temp.txt| jq '.country'`);
+	#c=$(echo `cat temp.txt | jq '.city'`);
+	#d=$(echo `cat temp.txt | jq '.region'`);
+	#e=$(echo `cat temp.txt | jq '.country'`);
+	c=$(echo $geo | jq '.city')
+	d=$(echo $geo | jq '.region')
+	e=$(echo $geo | jq '.country')	
+	
+	#set up for UA string encoding
+	ua_string=$(echo `cat actualHits.txt | grep "$b" |  sed s'/.$//' | sed 's/.*"//' | head -1`);
+	ua_string=$(urlencode $ua_string)
+	decoded_ua_string=$(curl -s "http://useragentapi.com/api/v2/json/$apikey/$ua_string");
+	#echo $decoded_ua_string
+	f=$(echo $decoded_ua_string | jq '.browser_name')
+	g=$(echo $decoded_ua_string | jq '.browser_version')
+	h=$(echo $decoded_ua_string | jq '.platform_name')
+	
 	echo $a $b $c $d $e >> visitorsLoc.txt;
+	echo $h $f $g >> visitorsLoc.txt;
 	cat actualHits.txt | grep "$b" | grep -E -o  " /.*?\.html | / " | sort | uniq >> visitorsLoc.txt
 	echo "----------------------------------" >> visitorsLoc.txt;
 done < goodVisitors.txt
